088f2a252d0a2a740b1ac1a0e47bd5d89e491eff
Elisa Boscani <elisabosc90@gmail.com>
2023-08-24 16:13:27 +0200

    add total price

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,10 @@
-const nameSurname = document.getElementById("nameSurname").value;
-const kilometres = document.getElementById("kilometres").value;
-const userAge = document.getElementById("userAge").value;
+const generates = document.querySelector("button");
 
-const price = kilometres * 0.21;
-console.log(price);
+generates.addEventListener("click", function () {
+  const kilometres = document.getElementById("kilometres").value;
+  const nameSurname = document.getElementById("nameSurname").value;
+  const userAge = document.getElementById("userAge").value;
+  const price = kilometres * 0.21;
+  console.log(price);
+  console.log(nameSurname, userAge);
+});
