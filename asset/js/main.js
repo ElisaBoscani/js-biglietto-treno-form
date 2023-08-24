c12f419a8c0f8b43a7ef81d278fd72096bfc5742
Elisa Boscani <elisabosc90@gmail.com>
2023-08-24 15:58:32 +0200

    add style

---
+++ asset/js/main.js
@@ -0,0 +1,6 @@
+const nameSurname = document.getElementById("nameSurname").value;
+const kilometres = document.getElementById("kilometres").value;
+const userAge = document.getElementById("userAge").value;
+
+const price = kilometres * 0.21;
+console.log(price);
